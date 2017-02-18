3cc0bffa8503369bc1711319d9d9ec8df89ba1c7
Rasmus-M <rasmus.moustgaard@gmail.com>
2017-02-18 23:54:15 +0100

    Version 5.8.4

--- src/js99er.js
+++ src/js99er.js
@@ -44,7 +44,7 @@
 
         log = Log.getLog();
         log.info("Welcome to JS99'er");
-        log.info("Version 5.8.3, 12 February 2016");
+        log.info("Version 5.8.4, 18 February 2016");
         log.info("- Added Megademo");
         log.info("");
         settings = new Settings(true);
