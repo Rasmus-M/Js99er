218e503a4a23909967115d46e2e3ffc04e7c32db
Rasmus-M <rasmus.moustgaard@gmail.com>
2018-02-04 17:22:09 +0100

    Tape sound

--- src/emu/tape.js
+++ src/emu/tape.js
@@ -9,11 +9,35 @@
 function Tape() {
     this.fileBuffer = null;
     this.playing = false;
+    this.audioContext = null;
+    if (window.AudioContext) {
+        this.audioContext = new AudioContext();
+    }
+    else if (window.webkitAudioContext) {
+        this.audioContext = new webkitAudioContext();
+    }
+    this.source = null;
     this.log = Log.getLog();
 }
 
 Tape.prototype.loadTapeFile = function (fileBuffer) {
     this.fileBuffer = fileBuffer;
+    var tape = this;
+    var audioContext = this.audioContext;
+    if (audioContext) {
+        audioContext.decodeAudioData(fileBuffer,
+            function (buffer) {
+                var source = audioContext.createBufferSource();
+                source.buffer = buffer;
+                source.connect(audioContext.destination);
+                source.loop = false;
+                tape.source = source;
+            },
+            function (e) {
+                this.log.error("Error decoding audio data" + e.err);
+            }
+        )
+    }
 };
 
 Tape.prototype.isTapeLoaded = function () {
@@ -22,8 +46,14 @@ Tape.prototype.isTapeLoaded = function () {
 
 Tape.prototype.play = function () {
     this.playing = true;
+    if (this.source) {
+        this.source.start(0);
+    }
 };
 
 Tape.prototype.stop = function () {
     this.playing = false;
+    if (this.source) {
+        this.source.stop();
+    }
 };
